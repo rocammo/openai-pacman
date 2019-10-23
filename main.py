37f30348f50f0538fb41b34932c9c369f23c0fa4
Rodrigo Casamayor <rodrigo.casamayor@gmail.com>
2019-10-23 18:03:21 +0200

    Getting Started: CartPole-v0 example loaded

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import gym
+
 def main():
-    print("Hello World!")
+    env = gym.make('CartPole-v0')
+    env.reset()
+    for _ in range(1000):
+        env.render()
+        env.step(env.action_space.sample()) # take a random action
+    env.close()
 
 if __name__ == "__main__":
     main()
