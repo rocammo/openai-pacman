3613d4b67e6cf1bd816a038fc7cebe918b73e1d7
i-rme <i-rme@users.noreply.github.com>
2019-12-17 14:11:40 +0100

    WIP, Successfully adapted the deep_q model into our framework (2)
    WIP, Successfully adapted the deep_q model into our framework

--- pacman/core/deep_Q.py
+++ pacman/core/deep_Q.py
@@ -9,76 +9,7 @@ from keras.optimizers import Adam
 from keras.models import Sequential
 from gym import wrappers
 
-class DeepQ(object):
-    def __init__(self, outer):
-        self.construct_q_network(outer)
-
-    def construct_q_network(self, outer):
-
-        EPISODES = 5
-        env = outer.env
-        # env = outer.env.wrappers.Monitor(env, '/tmp/MsPacman-ram-experiment-1',force=True)
-        # get size of state and action from environment
-        state_size = env.observation_space.shape[0]
-        action_size = env.action_space.n
-
-        agent = Agent(state_size, action_size)
-
-        scores, episodes = [], []
-
-        for e in range(EPISODES):
-            done = False
-            score = 0
-            state = env.reset()
-            state = np.reshape(state, [1, state_size])
-            lives = 3
-            while not done:
-                dead = False
-                while not dead:
-                    if agent.render:
-                        env.render()
-
-                    # get action for the current state and go one step in environment
-                    action = agent.get_action(state)
-                    next_state, reward, done, info = env.step(action)
-                    next_state = np.reshape(next_state, [1, state_size])
-                    # save the sample <s, a, r, s'> to the replay memory
-                    agent.append_sample(state, action, reward, next_state, done)
-                    # every time step do the training
-                    agent.train_model()
-
-                    state = next_state
-                    score += reward
-                    dead = info['ale.lives']<lives
-                    lives = info['ale.lives']
-                    # if an action make the Pacman dead, then gives penalty of -100
-                    reward = reward if not dead else -100
-
-                if done:
-                    scores.append(score)
-                    episodes.append(e)
-                    pylab.plot(episodes, scores, 'b')
-                    pylab.savefig("./pacman.png")
-                    print("episode:", e, "  score:", score, "  memory length:",
-                          len(agent.memory), "  epsilon:", agent.epsilon)
-
-
-            # save the model
-            if e % 50 == 0:
-                agent.model.save_weights("./pacman.h5")
-
-
-
-
-        print('Successfully constructed DDQN (DeepQ) network.')
-
-    def load_network(self, path):
-        # self.model = load_model(path)
-        print('Successfully loaded DDQN (DeepQ) network.')
-
-
-
-class Agent:
+class DeepQAgent:
     def __init__(self, state_size, action_size):
         # if you want to see MsPacman learning, then change to True
         self.render = True
@@ -166,5 +97,3 @@ class Agent:
         # and do the model fit!
         self.model.fit(update_input, target, batch_size=self.batch_size,
                        epochs=1, verbose=0)
-
-
